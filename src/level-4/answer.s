285813c83a031615e1ffb2ee3370070b9db3eb19
duandeyu <1843723137@qq.com>
2022-04-19 22:12:20 +0800

    not finish level4 and give up level5

---
+++ src/level-4/answer.s
@@ -0,0 +1,12 @@
+00000000004019a0 <addval_273>:
+  4019a0:	8d 87 48 89 c7 c3    	lea    -0x3c3876b8(%rdi),%eax
+  4019a6:	c3                   	retq   
+
+48 89 c7 c3 -> movq %rax, %rdi
+
+
+00000000004019a7 <addval_219>:
+  4019a7:	8d 87 51 73 58 90    	lea    -0x6fa78caf(%rdi),%eax
+  4019ad:	c3                   	retq   
+
+58 90 c3 -> popq %rax
